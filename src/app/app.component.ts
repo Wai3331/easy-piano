00b0f6df11d72b201b70d603dc8eaa5b78b49774
Wai3331 <98527717+Wai3331@users.noreply.github.com>
2022-01-27 23:04:13 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Tutorials', url: '/folder/Tutorials', icon: 'mail' },
+    { title: 'Database', url: '/folder/Database', icon: 'paper-plane' },
+    { title: 'Piano', url: '/folder/Piano', icon: 'heart' },
+    { title: 'Scoring', url: '/folder/Scoring', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Tutorials', 'Database', 'Piano', 'Scoring'];
   constructor() {}
 }
